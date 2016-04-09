3b0eeae4413da4be18af9561895847909cfe6e1c
LaurenCummings <LaurenCummings@users.noreply.github.com>
2016-04-09 10:11:23 -0400

    Added files via upload
    prevent calculation from being performed if width or height is set to 0

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@
 	app.controller('FormController', ['$scope', function($scope){
 		$scope.width = 0;
 		$scope.height = 0;
+		$scope.fabricSize = 16;
 
 		$scope.calculateSize = function() {
-			$scope.width = $scope.patternWidth / $scope.fabricSize;
-			$scope.height = $scope.patternHeight / $scope.fabricSize;
+			if($scope.patternWidth > 0 && $scope.patternHeight > 0){
+				$scope.width = $scope.patternWidth / $scope.fabricSize;
+				$scope.height = $scope.patternHeight / $scope.fabricSize;
+			}
 		};
+
 
 	}]);
 
